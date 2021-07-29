fe4022993b60159fb496aaa5e492f18f125fdd7f
Raphael <baisch474@gmail.com>
2021-07-29 09:11:54 -0300

    search bar with button css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { InputGroup, Input, InputGroupAddon, Button } from 'reactstrap';
 
 function App() {
   const mainHeader = () => {
@@ -10,9 +11,18 @@ function App() {
         <h1 className='display-2 text-center text-white mb-3' style={{ zIndex: 2 }}>
           Google Books
         </h1>
+        { /* SEARCH BAR */}
+        <div style={{ width: '60%', zIndex: 2 }}>
+          <InputGroup size='lg' className='mb-3'>
+            <Input placeholder='Book Search' />
+            <Button color='secondary'>
+              <i className='fas fa-search'></i>
+            </Button>
+          </InputGroup>
+        </div>
       </div>
-    )
-  }
+    );
+  };
   return (
     <div>
       {mainHeader()}
