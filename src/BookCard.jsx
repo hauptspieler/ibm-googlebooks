e1f2809095b621a5b5e6f112dd16eca258eed32e
Raphael <baisch474@gmail.com>
2021-07-29 17:14:33 -0300

    Modal pop-up with close x & ARIA support

--- src/BookCard.jsx
+++ src/BookCard.jsx
@@ -17,6 +17,9 @@ const BookCard = ({ thumbnail, title, pageCount, language, description, authors,
             <Modal isOpen={modal} toggle={toggle}>
                 <div className="modal-header d-flex justify-content-center">
                     <h5 className='modal-title text-center' id='exampleModalLabel'> {title}</h5>
+                    <button aria-label='Close' className='close' type='button' onClick={toggle}>
+                        <span aria-hidden={true}>X</span>
+                    </button>
                 </div>
             </Modal>
         </Card>
