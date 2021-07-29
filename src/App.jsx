4a0eefe03faee6c305fac6f5d1035d23da0e144d
Raphael <baisch474@gmail.com>
2021-07-29 13:42:36 -0300

    Handle Search with API and Toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { InputGroup, Input, InputGroupAddon, Button, FormGroup, Label } from 'reactstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import axios from 'axios'
+
 
 function App() {
-  // States
+  // STATE HOOKS [CURRENT STATE, FUNCTION TO UPDATE STATE]
+  const [maxResults, setMaxResults] = useState(10)
+  const [startIndex, setStartIndex] = useState(1)
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false)
+  const [cards, setCards ] = useState([])
+
+  // HANDLE SEARCH
+  const handleSubmit = () => {
+    setLoading(true);
+    if (maxResults > 40 || maxResults < 1) {
+      toast.error('max results must be between 1 and 40');
+    } else {
+      axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}&startIndex=${startIndex}`
+      )
+      .then(res => {
+        if (startIndex >= res.data.totalItems || startIndex < 1) {
+          toast.error(
+            `max reults must be between 1 and ${res.data.totalItems}`
+          );
+        } else {
+          if (res.data.items.length > 0) {
+            setCards(res.data.items);
+            setLoading(false);
+          }
+        }
+      })
+      .catch(err => {
+        setLoading(true);
+        console.log(err.response);
+      });
+  }
+};
+
+  // MAIN DISPLAY
   const mainHeader = () => {
     return (
       <div className='main-image d-flex justify-content-center align-items-center flex-column'>
@@ -15,29 +55,33 @@ function App() {
         { /* SEARCH BAR */}
         <div style={{ width: '60%', zIndex: 2 }}>
           <InputGroup size='lg' className='mb-3'>
-            <Input placeholder='Book Search' />
-            <Button color='secondary'>
+            <Input placeholder='Book Search' value={query} onChange={e => setQuery(e.target.value)} />
+            <Button color='secondary' onClick={handleSubmit}>
               <i className='fas fa-search'></i>
             </Button>
           </InputGroup>
           <div className="d-flex text-white justify-content-center">
-          { /* PAGE INDEXER */}
+            { /* PAGE INDEX */}
             <FormGroup className='ml-5'>
               <Label for='maxResults'>Max Results </Label>
-              <Input type='number' id='maxResults' placeholder='Max Results' />
+              <Input type='number' id='maxResults' placeholder='Max Results' value={maxResults} onChange={e => setMaxResults(e.target.value)} />
             </FormGroup>
             <FormGroup className='ml-5'>
               <Label for='startIndex'>Start Index </Label>
-              <Input type='number' id='startIndex' placeholder='Start Index' />
+              <Input type='number' id='startIndex' placeholder='Start Index' value={startIndex} onChange={e => setStartIndex(e.target.value)} />
             </FormGroup>
           </div>
         </div>
       </div>
     );
   };
+
+  
+
   return (
     <div>
       {mainHeader()}
+      < ToastContainer />
     </div>
   );
 }
